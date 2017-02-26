b9df9fa657a9d804f1f24700b1cee47d315faa4b
Ryan Hoegg <ryan.hoegg@gmail.com>
2017-02-26 15:02:08 -0600

    replace all System.setProperty calls with annotation configuration to prevent test isolation problems in Travis build

--- spring-cloud-vault-config/src/test/java/org/springframework/cloud/vault/config/VaultPropertySourceLocatorIntegrationTests.java
+++ spring-cloud-vault-config/src/test/java/org/springframework/cloud/vault/config/VaultPropertySourceLocatorIntegrationTests.java
@@ -24,15 +24,16 @@ import static org.assertj.core.api.Assertions.*;
  * @author Ryan Hoegg
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = VaultPropertySourceLocatorIntegrationTests.TestApplication.class)
+@SpringBootTest(classes = VaultPropertySourceLocatorIntegrationTests.TestApplication.class, properties = {
+	"spring.application.name=wintermute",
+	"spring.cloud.vault.application-name=neuromancer",
+	"spring.cloud.vault.generic.application-name=neuromancer,icebreaker"
+})
 @ActiveProfiles({"integrationtest"})
 public class VaultPropertySourceLocatorIntegrationTests extends IntegrationTestSupport {
 
 	@BeforeClass
 	public static void beforeClass() throws Exception {
-		System.setProperty("spring.application.name", "wintermute");
-		System.setProperty("spring.cloud.vault.application-name", "neuromancer");
-		System.setProperty("spring.cloud.vault.generic.application-name", "neuromancer,icebreaker");
 		VaultRule vaultRule = new VaultRule();
 		vaultRule.before();
 
